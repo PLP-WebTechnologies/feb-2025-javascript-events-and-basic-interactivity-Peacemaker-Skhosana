6c2eb7dceb44979942854c09fa82d0bb3583edda
Peacemaker-Skhosana <peacemakerskhosana@gmail.com>
2025-04-26 13:00:57 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,34 @@
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin: 0;
+  padding: 20px;
+  background-color: #f5f5f5;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+#clickButton {
+  background-color: lightblue;
+}
+
+#hoverButton {
+  background-color: lightgray;
+}
+
+#colorButton {
+  background-color: lightgreen;
+}
+
+#imageGallery {
+  margin-top: 20px;
+}
+
+#passwordFeedback {
+  font-size: 14px;
+  color: red;
+}
